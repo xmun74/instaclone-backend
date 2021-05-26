d83bc598e60536606f11125c5ce519ad80b77c04
Mun <xmun74@naver.com>
2021-05-26 16:01:40 +0900

    #3.12 Last Setup

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ require("dotenv").config();
 import { ApolloServer } from "apollo-server";
 import schema from "./schema";
 
+const PORT = process.env.PORT;
+//process.env로 PORT 접근함
 const server = new ApolloServer({
   schema,
 });
 
-const PORT = process.env.PORT;
-//process.env로 PORT 접근함
 server
   .listen(PORT)
   .then(() => console.log(`💖 Server is running on http://localhost:${PORT}/`));
